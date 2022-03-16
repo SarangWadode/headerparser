75b581bebd3dfad36c3b3cf4d552f496d48b059a
Sarang Wadode <sarangwadode@gmail.com>
2022-03-17 04:56:26 +0530

    test cases passed

--- server.js
+++ server.js
@@ -25,9 +25,20 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; 
+   const lang = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    "ipaddress": ip,
+    "language": lang,
+    "software": software
+  })
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const port = 3001;
+var listener = app.listen(port || process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
